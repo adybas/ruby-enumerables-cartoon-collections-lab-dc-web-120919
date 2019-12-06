0bb568534a2a06feda10222c6b7b5df108b9549f
Anna Dybas <a.m.dybas@gmail.com>
2019-12-06 00:57:13 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,6 +19,6 @@ def find_the_cheese(cheese_array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
   cheese_array.find do |array|
-    array.include?(cheese_array)
+    array.include?(cheese_array.to_s)
   end
 end
