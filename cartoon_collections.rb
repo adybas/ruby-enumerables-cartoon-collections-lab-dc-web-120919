cc7d50b468ea9054e963c282906ec9da9eb3ae94
Anna Dybas <a.m.dybas@gmail.com>
2019-12-06 00:49:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -15,7 +15,10 @@ def long_planeteer_calls(planeteer_array)# code an argument here
   planeteer_array.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese_array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_array.each do |array|
+    array.include?(cheese_array) ? : nil
+  end
 end
